e0c595dbc0817c0b08f324d81f17f372149e030f
Nguyen Hoang Gia <hoanggia042006@gmail.com>
2025-11-16 22:11:42 +0700

    Add DVD ID

--- AimsProject/src/com/hust/kstn/models/DigitalVideoDisc.java
+++ AimsProject/src/com/hust/kstn/models/DigitalVideoDisc.java
@@ -2,11 +2,13 @@ package com.hust.kstn.models;
 
 public class DigitalVideoDisc {
 
+    private static int nbDigitalVideoDiscs = 0;
     private String title;
     private String category;
     private String director;
     private int length;
     private double cost;
+    private int id;
 
     public String getTitle() {
         return title;
@@ -28,17 +30,28 @@ public class DigitalVideoDisc {
         return cost;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    private static int nextId() {
+        return ++nbDigitalVideoDiscs;
+    }
+
     public DigitalVideoDisc(String title) {
+        this.id = nextId();
         this.title = title;
     }
 
     public DigitalVideoDisc(String category, String title, double cost) {
+        this.id = nextId();
         this.title = title;
         this.category = category;
         this.cost = cost;
     }
 
     public DigitalVideoDisc(String director, String category, String title, double cost) {
+        this.id = nextId();
         this.director = director;
         this.category = category;
         this.title = title;
@@ -46,6 +59,7 @@ public class DigitalVideoDisc {
     }
 
     public DigitalVideoDisc(String title, String category, String director, int length, double cost) {
+        this.id = nextId();
         this.title = title;
         this.category = category;
         this.director = director;
